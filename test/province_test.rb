1bcab87b73817000fc9bda3fc70a3e79b06da527
boykj <jboyk918@gmail.com>
2021-05-16 18:59:07 -0700

    province_test.rb: add zero demand test

--- test/province_test.rb
+++ test/province_test.rb
@@ -6,7 +6,7 @@ require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
 
-  describe "basic_tests" do
+  describe "basic tests" do
 
     let(asia = Province.new(sample_province_data))
 
@@ -16,6 +16,12 @@ class ProvinceTest < Minitest::Test
 
     it "tests the province profit" do
       assert_equal(230, asia.profit)
+    end
+
+    it "test the zero demand" do
+      asia.demand = 0;
+      assert_equal(-25, asia.shortfall)
+      assert_equal(0, asia.profit)
     end
 
   end
