d8010442758b9f3fede02319d8527b20b868f7a4
boykj <jboyk918@gmail.com>
2021-05-16 19:08:20 -0700

    province_test.rb: introduce failing test for string method

--- test/province_test.rb
+++ test/province_test.rb
@@ -6,7 +6,7 @@ require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
 
-  describe "basic tests" do
+  describe "province tests" do
 
     let(asia = Province.new(sample_province_data))
 
@@ -18,10 +18,15 @@ class ProvinceTest < Minitest::Test
       assert_equal(230, asia.profit)
     end
 
-    it "test the zero demand" do
+    it "tests the zero demand" do
       asia.demand = 0;
       assert_equal(-25, asia.shortfall)
       assert_equal(0, asia.profit)
+    end
+
+    it "tests the string function" do
+      #asia.demand = ''
+      #assert_equal '', asia.demand
     end
 
   end
