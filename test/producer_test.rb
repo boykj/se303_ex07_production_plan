ed4f6c275a0e879f62a797eba3402cacefe59f27
boykj <jboyk918@gmail.com>
2021-05-16 19:54:29 -0700

    producer_test.rb: Add before(each) block for objects

--- test/producer_test.rb
+++ test/producer_test.rb
@@ -5,4 +5,18 @@ require_relative '../lib/producer'
 
 class ProducerTest < Minitest::Test
 
+	describe 'no producers' do
+
+		before(:each) {
+			DATA = {
+				name: "no producers",
+				producers: [],
+				demand: 30,
+				price: 20
+			}
+			let noProducers = Province.new(DATA)
+		}
+
+	end
+
 end
