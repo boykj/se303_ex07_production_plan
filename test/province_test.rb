17f1de5ba0a45c304741071006063cb961250c40
boykj <jboyk918@gmail.com>
2021-05-16 18:54:23 -0700

    province_test.rb: Add describe block: moved to using on object and testing via it blocks

--- test/province_test.rb
+++ test/province_test.rb
@@ -11,8 +11,23 @@ class ProvinceTest < Minitest::Test
     assert_equal(5, asia.shortfall)
   end
 
-  def test_prince_profit
+  def test_province_profit
     asia = Province.new(sample_province_data)
     assert_equal(230, asia.profit)
   end
+
+  describe "basic_tests" do
+
+    let(asia = Province.new(sample_province_data))
+
+    it "tests the province shortfall" do
+      assert_equal(5, asia.shortfall)
+    end
+
+    it "tests the province profit" do
+      assert_equal(230, asia.profit)
+    end
+
+  end
+
 end
